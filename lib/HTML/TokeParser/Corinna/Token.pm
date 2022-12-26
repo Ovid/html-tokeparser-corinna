557967bc253bf24fc3e3ce1100b8031964e9c6be
Ovid <ovid@cpan.org>
2022-12-26 16:31:08 +0100

    Add .perltidyrc with 'method' as a sub alias

--- lib/HTML/TokeParser/Corinna/Token.pm
+++ lib/HTML/TokeParser/Corinna/Token.pm
@@ -1,18 +1,21 @@
 use experimental 'class';
 
 class HTML::TokeParser::Corinna::Token {
+    no warnings 'experimental::builtin';
+    use builtin 'false';
+    use Perl::Tidy;
     field $token :param;
 
     method to_string              {$token->[1]}
     method _get_token             {$token}
-    method is_tag                 {}
-    method is_start_tag           {}
-    method is_end_tag             {}
-    method is_text                {}
-    method is_comment             {}
-    method is_declaration         {}
-    method is_pi                  {}
-    method is_process_instruction {}
+    method is_tag                 {false}
+    method is_start_tag           {false}
+    method is_end_tag             {false}
+    method is_text                {false}
+    method is_comment             {false}
+    method is_declaration         {false}
+    method is_pi                  {false}
+    method is_process_instruction {false}
     method rewrite_tag            {}
     method delete_attr            {}
     method set_attr               {}
